533064ee1e854472c04286578e37182c71c31473
dsheng1026 <di.sheng@pnnl.gov>
2024-12-03 15:53:54 -0500

    include a temporal resolution argument in HeatStress() function

--- R/HeatStress.R
+++ R/HeatStress.R
@@ -1,5 +1,6 @@
 #' HeatStress
 #'
+#' @param TempRes temporal resolution of input, "day" or "month"
 #' @param SECTOR choice of sector
 #' @param HS choice of heat stress function
 #' @param YEAR_INPUT a vector of years of interest
@@ -7,20 +8,26 @@
 #'
 #' @importFrom raster stack overlay calc stackApply as.matrix ncell nlayers
 #' @importFrom dplyr %>%
+#' @import assertthat
 #'
 #' @return A raster stack of daily grid-level heat stress level
 #' @export
 #'
 #' @examples
 #' \dontrun{
-#' GD_HS <- DAY2ANN(SECTOR = "MAIZ_I", HS = WBGT1, YEAR_INPUT = 2027,
+#' GD_HS <- DAY2ANN(TempRes = "day", SECTOR = "MAIZ_I", HS = WBGT1, YEAR_INPUT = 2027,
 #' "hurs_day_GFDL-ESM2M_rcp60_r1i1p1_EWEMBI_20210101-20301231.nc4",
 #' "tas_day_GFDL-ESM2M_rcp60_r1i1p1_EWEMBI_20210101-20301231.nc4",
 #' "ps_day_GFDL-ESM2M_rcp60_r1i1p1_EWEMBI_20210101-20301231.nc4")
 #' }
 
-HeatStress <- function(SECTOR, HS, YEAR_INPUT, ...){
-    # daily inputs
+HeatStress <- function(TempRes, SECTOR, HS, YEAR_INPUT, ...){
+
+  # check the input temporal resolution
+  if (missing(TempRes) || TempRes == "" || !TempRes %in% c("day", "mon")) {
+    stop("Error: Please specify TempRes: 'day' or 'month'.")
+  }
+
     climate_vars <- list(...)
     HS_arg_count <- length(formals(HS))
     if (length(climate_vars) != HS_arg_count) {
@@ -35,13 +42,11 @@ HeatStress <- function(SECTOR, HS, YEAR_INPUT, ...){
     SECTOR_INDEX <- which(SECTOR_ALL == SECTOR)
     sector_filter <- SECTOR_FLAG[[SECTOR_INDEX]]
 
-    # read in individual files of climate variables
-    for (i in seq_along(climate_vars)) {
-      assign(paste0("var", i,".stack"), raster::stack(climate_vars[[i]]))
-    }
-
-    # subset layers for year of interest
+    # Get the number of layers in the raster stack
     layer_names <- names(var1.stack)
+
+    # -----
+    # subset layers for year of interest
     layer_dates <- as.Date(gsub("X", "", layer_names), format = "%Y.%m.%d")
     YEAR <- format(layer_dates, "%Y")
 
@@ -50,11 +55,25 @@ HeatStress <- function(SECTOR, HS, YEAR_INPUT, ...){
       # If YEAR_INPUT is a subset of unique_years, proceed with the given YEAR_INPUT
     } else {
       # If X is not a subset of Y, stop with an error
-      stop("Error: invalid YEAR_INPUT, make sure input files include data for all years specified in YEAR_INPUT")
+      stop(paste0("Error: invalid YEAR_INPUT, make sure input files include data for ", YEAR_INPUT))
     }
 
     # subset layers falls into the year of interest
-    year_layer_index <- which(YEAR == YEAR_INPUT) # length of 365 or 366
+    year_layer_index <- which(YEAR == YEAR_INPUT)
+
+    # check number of layers
+    if (TempRes == "day") {
+      assert_that(
+        length(year_layer_index) %in% c(365, 366),
+        msg = paste0("Error: For daily input, expect 365 or 366 layers, but got ", length(year_layer_index), " layers.")
+      )
+    } else if (TempRes == "month") {
+      assert_that(
+        length(year_layer_index) == 12,
+        msg = paste0("Error: For monthly input, expect 12 layers, but got ", length(year_layer_index), " layers.")
+      )
+    }
+
     year_layer_names <- layer_names[year_layer_index]
     variable_list <- list()
 
@@ -69,5 +88,17 @@ HeatStress <- function(SECTOR, HS, YEAR_INPUT, ...){
     rm(variable_list)
     HS.stack.y <- stack(HS.stack.y)
     names(HS.stack.y) <- year_layer_names
+    # check number of layers
+    if (TempRes == "day") {
+      assert_that(
+        nlayers(HS.stack.y) %in% c(365, 366),
+        msg = paste0("Error: For daily input, expect 365 or 366 layers, but got ", nlayers(HS.stack.y), " layers.")
+      )
+    } else if (TempRes == "month") {
+      assert_that(
+        nlayers(HS.stack.y) == 12,
+        msg = paste0("Error: For monthly input, expect 12 layers, but got ", nlayers(HS.stack.y), " layers.")
+      )
+    }
     return(HS.stack.y)
   }
